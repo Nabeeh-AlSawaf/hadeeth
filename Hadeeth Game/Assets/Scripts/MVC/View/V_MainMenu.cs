e1a6dc3ba7789b1dff1d747e58d9612cf0c57045
Nabeeh-AlSawaf <61624303+Nabeeh-AlSawaf@users.noreply.github.com>
2020-08-05 12:28:43 +0300

    backend 1st step to start menu
    still need to do the share button
    and the toggles functionality

---
+++ Hadeeth Game/Assets/Scripts/MVC/View/V_MainMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class V_MainMenu : MonoBehaviour
+{
+    #region public vars
+    public GameObject settingsPanel;
+    public GameObject soundPanel;
+    public GameObject languagePanel;
+    public GameObject videoPanel;
+    public GameObject blurLayer;
+    #endregion
+    public void ClickQuit()
+    {
+        Application.Quit();
+    }
+    public void ClickStart()
+    {
+        SceneManager.LoadScene("Start");
+    }
+    public void ClickSettings()
+    {
+        blurLayer.SetActive(true);
+        settingsPanel.SetActive(true);
+
+    }
+    public void ClickShare()
+    {
+
+    }
+
+    public void ClickSound()
+    {
+        soundPanel.SetActive(true);
+        languagePanel.SetActive(false);
+        videoPanel.SetActive(false);
+    }
+
+    public void ClickLanguage()
+    {
+        soundPanel.SetActive(false);
+        languagePanel.SetActive(true);
+        videoPanel.SetActive(false);
+    }
+    public void ClickVideo()
+    {
+        soundPanel.SetActive(false);
+        languagePanel.SetActive(false);
+        videoPanel.SetActive(true);
+    }
+    public void ClickConfirm()
+    {
+        blurLayer.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+
+
+
+}
